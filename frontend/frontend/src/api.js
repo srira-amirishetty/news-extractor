5ca91b1fcbfe62a29d9ae5046978dd0f316be4c4
srira-amirishetty <sriramamirishetty@gmail.com>
2025-12-13 21:54:35 +0530

    .env for frontend

--- frontend/frontend/src/api.js
+++ frontend/frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000"; // your backend
+const API_URL = import.meta.env.VITE_API_URL; // your backend
 
 // Create axios instance (recommended)
 const api = axios.create({
